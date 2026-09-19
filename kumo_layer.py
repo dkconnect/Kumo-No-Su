71f48d51e74ffda66297c2b20e21cdda390d3d04
dkconnect <dkroyals1001@gmail.com>
2026-09-19 17:57:12 +0530

    Filename change & Creating Kumo Layer

---
+++ kumo_layer.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+
+class PolynomialBasis:
+    """Expands inputs into polynomial power tensors [x^0, x^1, x^2, ... x^d]."""
+
+    def __init__(self, degree=3):
+        self.degree = degree
+        self.D = degree + 1
+
+    def forward(self, X):
+        batch_size, in_features = X.shape
+        B = np.ones((batch_size, in_features, self.D))
+
+        for d in range(1, self.D):
+            B[:, :, d] = X**d
+        return B
+
+    def derivative(self, X):
+        batch_size, in_features = X.shape
+        dB_dX = np.zeros((batch_size, in_features, self.D))
+
+        for d in range(1, self.D):
+            dB_dX[:, :, d] = d * (X ** (d - 1))
+
+        return dB_dX
+
+class KumoLayer:
+    """Edge-based Learnable Polynomial Curve Layer."""
+
+    def __init__(self, in_features, out_features, degree=3):
+        self.in_features = in_features
+        self.out_features = out_features
+        self.degree = degree
+        self.D = degree + 1
+        self.basis = PolynomialBasis(degree)
+        self.C = (
+            np.random.randn(in_features, out_features, self.D)
+            * np.sqrt(2.0 / in_features)
+            * 0.1
+        )
+        self.b = np.zeros((1, out_features))
+
+    def forward(self, X):
+        self.X_in = X  
+        self.B = self.basis.forward(X)  
+
+        Y = np.einsum("nid,iod->no", self.B, self.C) + self.b
+        return Y
+
+    def backward(self, dL_dY, lr):
+        batch_size = dL_dY.shape[0]
+
+        dL_dC = np.einsum("nid,no->iod", self.B, dL_dY) / batch_size
+        dL_db = np.sum(dL_dY, axis=0, keepdims=True) / batch_size
+
+        dL_dB = np.einsum("no,iod->nid", dL_dY, self.C)
+        dB_dX = self.basis.derivative(self.X_in)
+        dL_dX = np.sum(dL_dB * dB_dX, axis=2)
+
+        self.C -= lr * dL_dC
+        self.b -= lr * dL_db
+
+        return dL_dX  
