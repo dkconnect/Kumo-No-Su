59ef477b3ed6dbe3a50442c045accd725814ae55
dkconnect <dkroyals1001@gmail.com>
2026-09-22 22:15:19 +0530

    Array

--- kumo_array.py
+++ kumo_array.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+x = 4
 C = np.array([
     [[1, 2, 3],
      [7, 8, 9]],
@@ -9,3 +10,11 @@ C = np.array([
 ])
 
 print(C.shape)
+
+powers = np.array([
+    x**0,
+    x**1,
+    x**2
+])
+
+print(powers)
