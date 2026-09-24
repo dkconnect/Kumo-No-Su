74f9f56fc91bf2d9754c676889c988f6e60f91cd
dkconnect <dkroyals1001@gmail.com>
2026-09-24 13:10:57 +0530

    Adding Bias

--- experiments/test_layer.py
+++ experiments/test_layer.py
@@ -10,4 +10,5 @@ layer = KumoLayer(
 x = np.array([2.0, 3.0])
 y = layer.forward(x)
 
+print("Bias:", layer.bias)
 print("Returned from Kumo:", y)
--- kumo/layer.py
+++ kumo/layer.py
@@ -6,7 +6,6 @@ class KumoLayer:
         self.n_inputs = n_inputs
         self.n_outputs = n_outputs
         self.degree = degree
-
         self.n_coeffs = degree + 1
 
         self.C = np.random.randn(
@@ -15,11 +14,13 @@ class KumoLayer:
             self.n_coeffs
         ) * 0.1
 
+        self.bias = np.zeros(n_outputs)
+
     def forward(self, x):
         powers = x[:, None, None] ** np.arange(self.n_coeffs)
         terms = self.C * powers
         edge_outputs = np.sum(terms, axis=2)
-        output = np.sum(edge_outputs, axis=0)
+        output = np.sum(edge_outputs, axis=0) + self.bias
 
         print("edge outputs:")
         print(edge_outputs)
