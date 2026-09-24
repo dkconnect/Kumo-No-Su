745e646f43b19bc9cdb071298ca90fa8bc7397fa
dkconnect <dkroyals1001@gmail.com>
2026-09-24 23:50:15 +0530

    remove self.bias

--- kumo/layer.py
+++ kumo/layer.py
@@ -14,22 +14,19 @@ class KumoLayer:
             self.n_coeffs
         ) * 0.1
 
-        self.bias = np.zeros(n_outputs)
-
     def forward(self, x):
         self.powers = x[:, None, None] ** np.arange(self.n_coeffs)
         terms = self.C * self.powers
 
         edge_outputs = np.sum(terms, axis=2)
-        output = np.sum(edge_outputs, axis=0) + self.bias
+        output = np.sum(edge_outputs, axis=0)
 
         return output
         
     def backward(self, error):
         coefficient_gradients = self.powers * error
-        bias_gradients = error
-        return coefficient_gradients, bias_gradients
+
+        return coefficient_gradients
 
     def update(self, coefficient_gradients, bias_gradients, learning_rate):
         self.C -= learning_rate * coefficient_gradients
-        self.bias -= learning_rate * bias_gradients
